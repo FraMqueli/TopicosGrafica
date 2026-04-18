c759b0e0489446f6d3149d90ea5537daabb470f3
Hunzart <hansjkuhnl@gmail.com>
2026-04-18 00:48:48 -0400

    Finish shadow render

--- tarea_1/src/render_shadow.py
+++ tarea_1/src/render_shadow.py
@@ -5,7 +5,7 @@ from dummy import Mesh
 epsilon = 1e-6
 
 # Implementación proyectando perspectiva de cada luz (más rápido)
-def render_shadow_project(mesh: Mesh, lights: list[dict] | None) -> np.array:
+def render_shadow_projection(mesh: Mesh, lights: list[dict] | None) -> np.array:
   if lights is None:
     lights = [{
       "type": "directional",
@@ -21,21 +21,91 @@ def render_shadow_project(mesh: Mesh, lights: list[dict] | None) -> np.array:
     lit_pixels = np.ones((256, 256), dtype=np.uint8)
     for face in mesh.faces:
       v0, v1, v2 = mesh.vertices[face]
-      d0, d1, d2 = (None, None, None)
+      projected = []
       if light["type"] == "directional":
-        d0, d1, d2 = (light["target"] - light["pos"], light["target"] - light["pos"], light["target"] - light["pos"])
+        dir = light["target"] - light["pos"]
+        projected = [(project_vertex_to_shadow_map(v0, dir), project_vertex_to_shadow_map(v1, dir), project_vertex_to_shadow_map(v2, dir))] 
+        if None in projected:
+          projected = []
       else:
-        d0, d1, d2 = (v0, v1, v2) - light["pos"]
-      v0 = project_vertex_to_shadow_map(v0, d0)
-      v1 = project_vertex_to_shadow_map(v1, d1)
-      v2 = project_vertex_to_shadow_map(v2, d2)
-      cover_pixels_in_light_shadow_map(lit_pixels, v0, v1, v2)
+        projected = check_clipping(v0, v1, v2, light["pos"])
+      
+      for p_vertexes in projected:
+        cover_pixels_in_light_shadow_map(lit_pixels, p_vertexes[0], p_vertexes[1], p_vertexes[2])
 
     for i in range(0, 256):
       for j in range(0, 256):
         if lit_pixels[i, j] == 1:
           shadow[i, j] = 1
   return shadow
+
+def check_clipping(v0, v1, v2, origin):
+  p0 = project_vertex_to_shadow_map(v0, v0 - origin)
+  p1 = project_vertex_to_shadow_map(v1, v1 - origin)
+  p2 = project_vertex_to_shadow_map(v2, v2 - origin)
+
+  if (p0 is not None and p1 is not None and p2 is not None):
+    return [(p0, p1, p2)]
+  
+  if (p0 is None and p1 is None and p2 is None):
+    return []
+
+  if (p0 is None and p1 is not None and p2 is not None):
+    return divide_triangle(v0, v1, v2, origin)
+
+  if (p0 is not None and p1 is None and p2 is not None):
+    return divide_triangle(v1, v0, v2, origin)
+  
+  if (p0 is not None and p1 is not None and p2 is None):
+    return divide_triangle(v2, v0, v1, origin)
+  
+  if (p0 is None and p1 is None and p2 is not None):
+    return trim_triangle(v0, v1, v2, origin)
+
+  if (p0 is not None and p1 is None and p2 is None):
+    return trim_triangle(v1, v2, v0, origin)
+  
+  if (p0 is None and p1 is not None and p2 is None):
+    return trim_triangle(v0, v2, v1, origin)
+  
+  #uh oh
+  return [(p0, p1, p2)]
+
+def divide_triangle(err, v0, v1, origin):
+  herr = err[2] - origin[2]
+  h0 = v0[2] - origin[2]
+  h1 = v1[2] - origin[2]
+  mid_u0 = abs(h0) / (herr + abs(h0)) - epsilon
+  mid_u1 = abs(h1) / (herr + abs(h1)) - epsilon
+  v2 = mid_u0*err + (1-mid_u0)*v0
+  v3 = mid_u1*err + (1-mid_u1)*v1
+
+  p0 = project_vertex_to_shadow_map(v0, v0-origin)
+  p1 = project_vertex_to_shadow_map(v1, v1-origin)
+  p2 = project_vertex_to_shadow_map(v2, v2-origin)
+  p3 = project_vertex_to_shadow_map(v3, v3-origin)
+
+  return [
+    (p0, p1, p2),
+    (p0, p1, p3)
+  ]
+
+def trim_triangle(err1, err2, v0, origin):
+  herr1 = err1[2] - origin[2]
+  herr2 = err2[2] - origin[2]
+  h0 = v0[2] - origin[2]
+  mid_u1 = abs(h0) / (herr1 + abs(h0)) - epsilon
+  mid_u2 = abs(h0) / (herr2 + abs(h0)) - epsilon
+  v1 = mid_u1*err1 + (1-mid_u1)*v0
+  v2 = mid_u2*err2 + (1-mid_u2)*v0
+
+  p0 = project_vertex_to_shadow_map(v0, v0-origin)
+  p1 = project_vertex_to_shadow_map(v1, v1-origin)
+  p2 = project_vertex_to_shadow_map(v2, v2-origin)
+
+  return [
+    (p0, p1, p2)
+  ]
 
 def project_vertex_to_shadow_map(vertex, direction):
   if vertex[2] > 0 and direction[2] > 0 or vertex[2] < 0 and direction[2] < 0 or vertex[2] != 0 and direction[2] == 0:
@@ -54,8 +124,8 @@ def cover_pixels_in_light_shadow_map(lit_pixels, v0, v1, v2):
   y_min = int(min(p0[1], p1[1], p2[1]))
   y_max = int(max(p0[1], p1[1], p2[1]))
 
-  for i in range(max(x_min, 0), min(x_max+1, 256)):
-    for j in range(max(y_min, 0), min(y_max+1, 256)):
+  for i in range(max(y_min, 0), min(y_max+1, 256)):
+    for j in range(max(x_min, 0), min(x_max+1, 256)):
       if(lit_pixels[i, j] == 0):
         continue
       if(point_in_triangle(np.array([i, j, 0]), p0, p1, p2)):
@@ -98,9 +168,10 @@ def render_shadow_ray(mesh: Mesh, lights: list[dict] | None) -> np.array:
 
   shadow = np.zeros((256, 256), dtype=np.uint8)
   for i in range(256):
+    print(i)
     for j in range(256):
-      x_coord = pixel_size*(i + 0.5) - 0.6
-      y_coord = pixel_size*(j + 0.5) - 0.6
+      x_coord = pixel_size*(j + 0.5) - 0.6
+      y_coord = pixel_size*(i + 0.5) - 0.6
       point = np.array([x_coord, y_coord, 0])
 
       for light in lights:
@@ -118,6 +189,7 @@ def render_shadow_ray(mesh: Mesh, lights: list[dict] | None) -> np.array:
         if not obstructed:
           shadow[i, j] = 1
           break
+          
   return shadow
 
 def ray_triangle_intersect(ray_origin, ray_direction, v0, v1, v2):
@@ -125,7 +197,7 @@ def ray_triangle_intersect(ray_origin, ray_direction, v0, v1, v2):
   # https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
   edge1 = v1 - v0
   edge2 = v2 - v0
-  triangle_normal = np.cross(edge1, edge2)
+
   ray_cross_e2 = np.cross(ray_direction, edge2)
   det = np.dot(edge1, ray_cross_e2)
   if(abs(det) < epsilon):
@@ -141,7 +213,7 @@ def ray_triangle_intersect(ray_origin, ray_direction, v0, v1, v2):
   s_cross_e1 = np.cross(s, edge1)
   v = inv_det*np.dot(ray_direction, s_cross_e1)
 
-  if u < -epsilon or u+v-1 > epsilon:
+  if v < -epsilon or u+v-1 > epsilon:
     return False
   
   return True
